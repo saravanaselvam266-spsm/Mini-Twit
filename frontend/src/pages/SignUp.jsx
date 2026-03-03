5fb7211deb19dc5b920fe785c6b97188cd9befc9
saravanaselvam266-spsm <saravana.selvam266@gmail.com>
2026-03-03 15:25:17 +0530

    logo added

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,43 +2,60 @@ import React from "react";
 import Logo from "../assets/twitter_image.png";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const SignUp = () => {
   return (
-    <div className="flex flex-col h-[100vh] justify-center items-center ">
-      <form className="p-[30px] h-[500px] shadow-2xl">
+    <div className="flex flex-col h-[100vh] justify-center items-center">
+      <form className="p-[30px] h-[620px] shadow-2xl">
         <h1 className="flex justify-center items-center text-[30px] font-bold">
           <img src={Logo} className="w-10 h-10 rounded-[50%]" />
           &nbsp;Mini Twit
         </h1>
         <h2 className="flex justify-center items-center text-[20px] font-bold">
-          <strong>Welcome back</strong>
+          <strong>Create your account</strong>
         </h2>
         <p className="flex justify-center items-center mb-[20px]">
-          Enter your credentials to access your feed
+          join the community and start sharing.
         </p>
-        <label htmlFor="email">Email address</label>
+        <label htmlFor="name">Full Name</label>
         <br />
         <input
           className="border p-[8px] w-[350px] rounded-[5px] mb-[10px] mt-1.5"
           type="text"
+          placeholder="Enter your full name "
+        />
+        <br />
+        <label htmlFor="username">Username</label>
+        <br />
+        <input
+          className="border p-[8px] w-[350px] rounded-[5px] mb-[10px] mt-1.5"
+          type="username"
+          placeholder="Choose a unique username"
+        />
+        <br />
+        <label htmlFor="email">Email Address</label>
+        <br />
+        <input
+          className="border p-[8px] w-[350px] rounded-[5px] mb-[10px] mt-1.5"
+          type="email"
           placeholder="name@example.com"
         />
         <br />
-        <label htmlFor="password">Password </label>
+        <label htmlFor="password">Password</label>
         <br />
         <input
           className="border p-[8px] w-[350px] rounded-[5px] mb-[10px] mt-1.5"
           type="password"
-          placeholder="Enter your password"
+          placeholder="Create a strong Password"
         />
-        <p>Forget Password?</p>
+        <br />
         <button className="border w-[350px] h-[44px] p-[7px] rounded-[5px] mt-[40px] bg-[#7C3AEDFF] text-[#FFFFFFFF]">
-          Sign In
+          Sign Up
         </button>
+        <br />
         <p className="flex justify-center items-center m-[10px] ">
-          Don't have an account?&nbsp;
+          Already have an account?&nbsp;
           <strong>
-            <Link to={"/signup"}>Sign Up</Link>
+            <Link to={"/signin"}>Sign In</Link>
           </strong>
         </p>
       </form>
@@ -46,4 +63,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
